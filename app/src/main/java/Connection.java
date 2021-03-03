8553a279c06c659ccfb764e5aea2b1667c4df406
Anthony Monkov <antowa.monkov@gmail.com>
2021-03-03 22:28:41 +0200

    feat: temp add build

--- app/src/main/java/Connection.java
+++ app/src/main/java/Connection.java
@@ -54,7 +54,7 @@ class Connection{
                 .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
                 .setAccessType("offline")
                 .build();
-        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setHost("165.22.75.15").setPort(8888).build();
         return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
     }
 
